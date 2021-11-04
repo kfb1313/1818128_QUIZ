1a2daa5b0c0ec1ea3188d87f91bf7ca67dca8a5f
kfb1313 <kevinfb47@gmail.com>
2021-11-04 12:49:02 +0700

    First commit

--- js/script.js
+++ js/script.js
@@ -1,124 +1,72 @@
-const ApiKey = "5a895546d25846169199c0bb48f65fd4";
-const baseUrl = "https://api.football-data.org/v2/";
-const leagueId = "2021";
-const baseEndPoin = `${baseUrl}competitions/${leagueId}`;
-const teamEndPoin = `${baseUrl}competitions/${leagueId}/teams`;
-const standingEndPoin = `${baseUrl}competitions/${leagueId}/standings`;
-const matchEndPoin = `${baseUrl}competitions/${leagueId}/matches`;
+const ApiKey = "";
+const baseUrl = "http://ergast.com/api/f1/";
+const season = "2021";
+const baseEndPoin = `${baseUrl}${season}`;
+const driverEndPoin = `${baseUrl}${season}/drivers.json`;
+const standingEndPoin = `${baseUrl}${season}/driverStandings.json`;
+const constructorEndPoin = `${baseUrl}${season}/constructors.json`;
 
 const contents = document.querySelector("#content-list");
 const title = document.querySelector(".card-title");
-const fetchHeader = {
-    headers: {
-        'X-Auth-Token': ApiKey
-    }
-};
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
 
-function getListTeams() {
-    title.innerHTML = "Daftar Tim Liga Primer Inggris"
-    fetch(teamEndPoin, fetchHeader)
-        .then(response => response.json())
-        .then(resJson => {
-            console.log(resJson.teams);
-            let teams = "";
-            resJson.teams.forEach(team => {
-                teams += `
-                <li class="collection-item avatar">
-                    <img src="${team.crestUrl}" alt="" class="circle">
-                    <span class="title">${team.name}</span>
-                    <p>Berdiri: ${team.founded} <br>
-                       Markas: ${team.venue}
-                    </p>
-                    <a href="#${team.id}" data-id="${team.id}"class="secondary-content"><i class="material-icons" data-id="${team.id}">info</i></a>
-                </li>
-                `
-            });
-            contents.innerHTML = '<ul class="collection">' + teams + '</ul>'
-            const detail = document.querySelectorAll('.secondary-content');
-            detail.forEach(btn => {
-                btn.onclick=(event) =>{
-                    loadPage(event.target.dataset.id);
-
-                }
-            })
-                
-        }).catch(err => {
-            console.error(err);
-        })
-}
-
-function ShowTeaminfo(page)
-{
-    let url=baseUrl+ "teams/"+page;
-    title.innerHTML = "Informasi Team"
-    fetch(url, fetchHeader)
-        .then(response => response.json())
-        .then(resJson => {
-            console.log(resJson);
-            contents.innerHTML=`
-            <div class="row">
-    <div class="col s12 m6">
-      <div class="card">
-        <div class="card-image">
-          <img src="${resJson.crestUrl}">
-          <span class="card-title">Card Title</span>
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-        </div>
-        <div class="card-content">
-          <p>Nama Team: ${resJson.shortName}<br>
-          Tahun Berdiri:${resJson.founded}<br>
-          Official:${resJson.venue}<br>
-          Alamat:${resJson.addres}<br>
-          Email:${resJson.email}<br>
-          Website:${resJson.website}<br>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-            `;
+function getListDrivers() {
+    title.innerHTML = "Daftar Driver 2021"
+    fetch(driverEndPoin, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result.MRData.DriverTable.Drivers);
+        let Drivers = "";
+        result.MRData.DriverTable.Drivers.forEach(Drivers => {
+            Drivers += `
+            <li class="collection-item avatar">
+                <span class="title"> ${Drivers.givenName} </span>
+                <p> Nomor Mobil: ${Drivers.permanentNumber} <br>
+                    Tanggal Lahir: ${Drivers.dateOfBirth} <br>
+                    Asal Negara: ${Drivers.nationality}
+                </p>
+            </li>
+            `
         });
+        contents.innerHTML = '<ul class="collection">' + result.MRData.DriverTable.Drivers + '</ul>'
+    })
+    .catch(error => console.log('error', error));
 }
 
 function getListStandings() {
-    title.innerHTML = "Klasemen Sementara Liga Primer Inggris";
-    fetch(standingEndPoin, fetchHeader)
-        .then(response => response.json())
-        .then(resJson => {
-            console.log(resJson.standings[0]);
-            let teams = "";
+    title.innerHTML = "2021 F1 Standings"
+    fetch(standingEndPoin, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+            console.log(result.MRData.StandingsTable.StandingsList.DriverStandings);
+            let Standings = "";
             let i = 1;
-            resJson.standings[0].table.forEach(team => {
-                teams += `
+            result.MRData.StandingsTable.StandingsList.DriverStandings.forEach(Standings => {
+                Standings += `
                 <tr>
                     <td style="padding-left:20px;">${i}.</td>
-                    <td><img src="${team.team.crestUrl}" alt="${team.team.name}" width="30px"></td>
-                    <td>${team.team.name}</td>
-                    <td>${team.playedGames}</td>
-                    <td>${team.won}</td>
-                    <td>${team.draw}</td>
-                    <td>${team.lost}</td>
-                    <td>${team.points}</td>
+                    <td>${Standings.position}</td>
+                    <td>${Standings.points}</td>
+                    <td>${Standings.wins}</td>
+                    <td>${Standings.Driver.givenName}</td>
                 </tr>
                 `;
                 i++;
-
             });
             contents.innerHTML = `
                 <div class="card">
                     <table class="stripped responsive-table">
                         <thead>
-                            <th></th>
-                            <th></th>
-                            <th>Nama Tim</th>
-                            <th>PG</th>
-                            <th>W</th>
-                            <th>D</th>
-                            <th>L</th>
-                            <th>P</th>
+                            <th>Posisi</th>
+                            <th>Poin Diperoleh</th>
+                            <th>Menang</th>
+                            <th>Driver</th>
                         </thead>
                         <tbody>
-                            ${teams}
+                            ${Driver}
                         </tbody>
                     </table>
                 </div>
@@ -128,79 +76,30 @@ function getListStandings() {
         })
 }
 
-function getListMatches() {
-    title.innerHTML = "Jadwal Pertandingan Liga Primer Inggris";
-    fetch(matchEndPoin, fetchHeader)
-        .then(response => response.json())
-        .then(resJson => {
-            console.log(resJson.matches);
-            let matchs = "";
-            let i = 1;
-            resJson.matches.forEach(match => {
-                let d = new Date(match.utcDate).toLocaleDateString("id");
-                let scoreHomeTeam = (match.score.fullTime.homeTeam == null ? 0 : match.score.fullTime.homeTeam);
-                let scoreAwayTeam = (match.score.fullTime.awayTeam == null ? 0 : match.score.fullTime.awayTeam);
-                matchs += `
-                <tr>
-                    <td style="padding-left:20px;">${i}.</td>
-                    <td>${match.homeTeam.name} vs ${match.awayTeam.name}</td>
-                    <td>${d}</td>
-                    <td>${scoreHomeTeam}:${scoreAwayTeam}</td>
-                </tr>
-                `;
-                i++;
-
-            });
-            contents.innerHTML = `
-                <div class="card">
-                    <table class="stripped responsive-table">
-                        <thead>
-                            <th></th>
-                            <th>Peserta</th>
-                            <th>Tanggal</th>
-                            <th>Skor Akhir</th>
-                        </thead>
-                        <tbody>
-                            ${matchs}
-                        </tbody>
-                    </table>
-                </div>
-            `;
-        }).catch(err => {
-            console.error(err);
-        })
-}
 
 function loadPage(page) {
     switch (page) {
-        case "teams":
-            getListTeams();
+        case "Drivers":
+            getListDrivers();
             break;
-        case "standings":
+        case "Standings":
             getListStandings();
             break;
-        case "matches":
-            getListMatches();
-            break;
-        case page:
-            ShowTeaminfo(page);
-            break;
-    }
+    } 
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
-
     document.querySelectorAll(".sidenav a, .topnav a").forEach(elm => {
         elm.addEventListener("click", evt => {
             let sideNav = document.querySelector(".sidenav");
             M.Sidenav.getInstance(sideNav).close();
             page = evt.target.getAttribute("href").substr(1);
-            loadPage(page);
+            console.log(page);
         })
     })
     var page = window.location.hash.substr(1);
-    if (page === "" || page === "!") page = "teams";
+    if (page === "" || page === "!") page = "Drivers";
     loadPage(page);
 });
